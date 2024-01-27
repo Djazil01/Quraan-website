2b1366408056b818e6a2218b3dbaeff3ce28ec96
unknown <oukilidjazil@gmail.com>
2024-01-28 00:44:51 +0100

    commit2

--- main.js
+++ main.js
@@ -20,6 +20,73 @@ function getsurahs() {
         
         `;
       }
+      let allsurahs = document.querySelectorAll(".surahs div"),
+        ayahsaudios,
+        ayahstext;
+      allsurahs.forEach((surah, index) => {
+        surah.addEventListener("click", () => {
+          fetch(`https://api.quran.gading.dev/surah/${index + 1}`)
+            .then((response) => response.json())
+            .then((data) => {
+              let verses = data.data.verses;
+              ayahsaudios = [];
+              ayahstext = [];
+              verses.forEach((verses) => {
+                ayahsaudios.push(verses.audio.primary);
+                ayahstext.push(verses.text.arab);
+              });
+              let ayahindex = 0;
+              changeayah(ayahindex);
+              audio.addEventListener("ended", () => {
+                ayahindex++;
+                if (ayahindex < ayahsaudios.length) {
+                  changeayah(ayahindex);
+                } else {
+                  Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "surah has been ended",
+                    showConfirmButton: false,
+                    timer: 1500,
+                  });
+                  isplaying = true;
+                  toggelplay();
+                  ayahindex = 0;
+                  changeayah(ayahindex);
+                  audio.pause();
+                }
+              });
+              next.addEventListener("click", () => {
+                ayahindex < ayahsaudios.length - 1
+                  ? ayahindex++
+                  : (ayahindex = 0);
+                changeayah(ayahindex);
+              });
+              prev.addEventListener("click", () => {
+                ayahindex > 0 ? ayahindex-- : (ayahindex = 0);
+                changeayah(ayahindex);
+              });
+              let isplaying = false;
+              toggelplay();
+              function toggelplay() {
+                if (isplaying) {
+                  audio.pause();
+                  play.innerHTML = `<i class="fas fa-play"></i>`;
+                  isplaying = false;
+                } else {
+                  audio.play();
+                  play.innerHTML = `<i class="fas fa-pause"></i>`;
+                  isplaying = true;
+                }
+              }
+              play.addEventListener("click", toggelplay);
+              function changeayah(index) {
+                audio.src = ayahsaudios[index];
+                ayah.innerHTML = ayahstext[index];
+              }
+            });
+        });
+      });
     });
 }
 getsurahs();
